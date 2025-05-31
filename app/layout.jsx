8bbc458f212311be149708f91f1ef169a0a895d7
Arman <contact.armankar@gmail.com>
2025-05-31 14:42:38 +0530

    code refactor and new experimental ui

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { ReactScan } from "@/lib/react-scan";
+import "./globals.css";
+import { geistSans, geistMono } from "./fonts";
+
+export const metadata = {
+  title: "Student - Developer",
+  icons: {
+    icon: "/images/converted.ico",
+    shortcut: "/images/converted.ico",
+  },
+  description:
+    "Portfolio of Arman Kar, full stack developer and AI enthusiast. Explore web projects, real-time apps, and tech content. Connect on GitHub, LinkedIn, and Twitter.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <ReactScan />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
