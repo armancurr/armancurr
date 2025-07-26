4de9a11c757e9a4490052f85dc77e7c395ef8212
Arman <armancurr@proton.me>
2025-07-26 14:05:11 +0530

    feat(content): add content types & data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-// @ts-check
-
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+import react from "@astrojs/react";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  server: {
+    port: 3000,
+  },
+
+  site: "https://armancurr.vercel.app",
+  integrations: [react()],
 });
