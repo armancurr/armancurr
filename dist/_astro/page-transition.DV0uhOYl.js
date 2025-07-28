c3fb78be5b41e26c709e75795ec57790bb1a2211
Arman <armancurr@proton.me>
2025-07-28 21:02:29 +0530

    feat(astro): update settings & types
    Refactor: Update package versions.

--- dist/_astro/page-transition.DV0uhOYl.js
+++ dist/_astro/page-transition.DV0uhOYl.js
@@ -1,1 +1,3 @@
-import{default as e}from"./page-transition.BHPoTJ5_.js";import"./index.9MVAkNgL.js";export{e as default};
+import { default as e } from "./page-transition.BHPoTJ5_.js";
+import "./index.9MVAkNgL.js";
+export { e as default };
